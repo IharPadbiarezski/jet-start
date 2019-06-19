b5485eda6f46c438b9736d968d3a202df2de774e
Ihar <yb.igrok.by@gmail.com>
2019-06-19 23:07:42 +0300

    feat: add basic layout of contacts page

--- sources/views/start.js
+++ sources/views/start.js
@@ -1,3 +1,35 @@
-export default {
-	template: "Start page", css: "webix_shadow_medium app_start"
-};
+import {JetView} from "webix-jet";
+import {contacts} from "../models/contacts";
+
+export default class ContactsView extends JetView {
+	config() {
+		return {
+			cols: [
+				{
+					rows: [
+						{view: "label", label: "Contacts", align: "center", localId: "label", css: "contact_label"},
+						{
+							view: "list", localId: "list", scroll: false, template: "#Name# - #Email#  <span class='webix_icon wxi-close rmvicon'></span>"
+						}
+					]
+				},
+				{
+					view: "form",
+					elements: [
+						{
+							view: "text",	name: "user_name", label: "User Name"
+						},
+						{
+							view: "text", name: "email", label: "Email"
+						},
+						{}
+					]
+				}
+			]};
+	}
+
+	init() {
+		const list = this.$$("list");
+		list.parse(contacts);
+	}
+}
