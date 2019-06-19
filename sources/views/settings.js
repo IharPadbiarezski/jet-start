d3cc10f46f6d75caca9f549f0b8cd410dd1668c1
Ihar <yb.igrok.by@gmail.com>
2019-06-19 23:52:22 +0300

    feat: display segmented button on settings page

--- sources/views/settings.js
+++ sources/views/settings.js
@@ -1,3 +1,21 @@
-export default {
-	template: "Settings page"
-};
+import {JetView} from "webix-jet";
+
+export default class SettingsView extends JetView {
+	config() {
+		return {
+			rows: [
+				{view: "label", label: "Settings", align: "center", localId: "label", css: "settings_label"},
+				{view: "segmented",
+					value: "ru",
+					inputWidth: 250,
+					options: [
+						{id: "ru", value: "RU"},
+						{id: "en", value: "EN"}
+					]}
+			]
+		};
+	}
+
+	init() {
+	}
+}
