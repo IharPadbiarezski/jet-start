362ebd7eb4061776088d756a4e2efb5208e47231
Ihar <yb.igrok.by@gmail.com>
2019-06-20 10:33:13 +0300

    feat: display data layout

--- sources/views/data.js
+++ sources/views/data.js
@@ -1,3 +1,68 @@
-export default {
-	template: "Data page"
-};
+import {JetView} from "webix-jet";
+import {countries} from "../models/countries";
+import {statuses} from "../models/statuses";
+
+export default class DataView extends JetView {
+	config() {
+		let tabbar = {
+			view: "tabbar",
+			multiview: true,
+			animate: {type: "flip", direction: "top"},
+			options: [
+				{
+					value: "Countries",
+					id: "countries_view"
+				},
+				{
+					value: "Statuses",
+					id: "statuses_view"
+				}
+			],
+			height: 50
+		};
+
+		let countriesView = {
+			id: "countries_view",
+			view: "datatable",
+			css: "align-center",
+			scroll: "y",
+			columns: [
+				{id: "id", header: "", width: 50, sort: "int"},
+				{id: "Name", header: "Name", fillspace: true, sort: "string"}
+			]
+		};
+
+		let statusesView = {
+			id: "statuses_view",
+			view: "datatable",
+			scroll: "y",
+			css: "align-center",
+			columns: [
+				{id: "Name", header: "Name", fillspace: true, sort: "string"},
+				{id: "Icon", header: "Icon", fillspace: true, sort: "string"}
+			]
+		};
+
+
+		return {
+			rows: [
+				{view: "label", label: "Data", align: "center", localId: "label", css: "contact_label"},
+				tabbar,
+				{
+					animate: false,
+					cells: [
+						countriesView, statusesView
+					]
+				},
+				{cols: [{view: "button", value: "Add"}, {view: "button", value: "Delete"}]}
+			]
+		};
+	}
+
+	init() {
+		const countriesView = this.$$("countries_view");
+		countriesView.parse(countries);
+		const statusesView = this.$$("statuses_view");
+		statusesView.parse(statuses);
+	}
+}
